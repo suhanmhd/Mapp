4a599168b1291f3a81e74c459958f391a21826d5
suhanmhd <68009966+suhanmhd@users.noreply.github.com>
2020-07-21 22:52:59 -0700

    counter app ]

---
+++ app/src/main/java/com/example/mapp/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.mapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    int myCountvalue = 0;
+    Button btMinus, btPlus;
+    TextView txCount, txReset;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        btMinus = findViewById(R.id.btMinus);
+        btPlus = findViewById(R.id.btPlus);
+        txCount = findViewById(R.id.txCount);
+        txReset = findViewById(R.id.txReset);
+
+        btMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (myCountvalue == 0) {
+
+                } else {
+                    myCountvalue = myCountvalue - 1;
+                    txCount.setText(myCountvalue + "");
+                }
+            }
+        });
+        btPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myCountvalue = myCountvalue + 1;
+                txCount.setText(myCountvalue + "");
+            }
+        });
+        txReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myCountvalue = 0;
+                txCount.setText(myCountvalue + "");
+            }
+        });
+    }
+}
